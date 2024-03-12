e1dbe52a327dae6343245ef1e9528641eeb9618d
devsathwara-enacton <devsath.enacton@gmail.com>
2024-03-12 18:06:21 +0530

    swagger schema integrate done

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,4 +1,12 @@
 import { createJsonSchemaTransform } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { OpenAPIV3 } from "openapi-types";
+import {
+  userSchema,
+  offerWallTasksSchema,
+  offerWallNetworkSchema,
+  offerWallCategories,
+} from "./swaggerSchema";
 
 export const swaggerOptions = {
   openapi: {
@@ -6,9 +14,17 @@ export const swaggerOptions = {
       title: "Freecash API",
       description: "Cash earning platform",
       version: "1.0.0",
-    },
-    servers: [],
-  },
+    } as OpenAPIV3.InfoObject,
+    servers: [] as OpenAPIV3.ServerObject[],
+    components: {
+      schemas: {
+        User: userSchema as OpenAPIV3.SchemaObject,
+        Tasks: offerWallTasksSchema as OpenAPIV3.SchemaObject,
+        Networks: offerWallNetworkSchema as OpenAPIV3.SchemaObject,
+        Categories: offerWallCategories as OpenAPIV3.SchemaObject,
+      },
+    } as OpenAPIV3.ComponentsObject,
+  } as OpenAPIV3.Document,
   transform: createJsonSchemaTransform({
     skipList: ["/api/v1/auth/google", "/api/v1/auth/facebook"],
   }),
